789e5339293003d22cea47a8d3f0b1ddde8107c0
Jon Kaplon <jon.kaplon1@gmail.com>
2022-03-24 15:14:09 -0700

    commit tree ede23580cfad4912dc2e87615a4a691d4f8b581d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,36 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   get '/' do
-    { message: "Hello world" }.to_json
+    { message: 'Hello world' }.to_json
   end
 
+  get '/games' do
+    # get all games from the database
+    games = Game.all.order(:title).limit(5)
+
+    #return a JSON respons with an array of all the game data
+    games.to_json
+  end
+
+  get '/games/:id' do
+    #look up the game in the database using its id
+    game = Game.find(params[:id])
+
+    #send a JSON-formmatted response of the game data
+    # game.to_json(include: { reviews: { include: :user } })
+    game.to_json(
+      only: %i[id title genre price],
+      include: {
+        reviews: {
+          only: %i[comment score],
+          include: {
+            user: {
+              only: [:name],
+            },
+          },
+        },
+      },
+    )
+  end
 end
